305b5bda69ff61c90b32301372292c11c75cd7ec
viktorcicd <94958618+viktorcicd@users.noreply.github.com>
2022-02-15 14:07:50 +0200

    Update Application.java

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -18,7 +18,7 @@ public class Application {
     public void init()
     {
         Logger log = LoggerFactory.getLogger(Application.class);
-        log.info("Java app started");
+        log.info("myJava app started");
     }
 
     public String getStatus() {
